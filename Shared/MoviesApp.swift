af1e0fda5e176d452f9745095413ae1e35bf0c5d
MojitoBar <wnehdtjr5@gmail.com>
2021-02-02 10:48:06 +0900

    Initial Commit

---
+++ Shared/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Shared
+//
+//  Created by judongseok on 2021/02/02.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
